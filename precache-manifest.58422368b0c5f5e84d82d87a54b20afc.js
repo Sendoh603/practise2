edc1c1d9c695084da37c39240f5ff92e9c5925fc
Wei Zhang <sheepchina@hotmail.com>
2020-01-27 04:03:50 -0600

    Add all

---
+++ precache-manifest.58422368b0c5f5e84d82d87a54b20afc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a11fd704c85da1f4f24d8170b07b94",
+    "url": "/assignment2/index.html"
+  },
+  {
+    "revision": "a349c5b307e16103f0d3",
+    "url": "/assignment2/static/css/main.3e7e3649.chunk.css"
+  },
+  {
+    "revision": "d95f37056c9d9eab6a47",
+    "url": "/assignment2/static/js/2.c1f04fe4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/assignment2/static/js/2.c1f04fe4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a349c5b307e16103f0d3",
+    "url": "/assignment2/static/js/main.1d9cf18a.chunk.js"
+  },
+  {
+    "revision": "faba956f00397628b9b6",
+    "url": "/assignment2/static/js/runtime-main.21c0ddf3.js"
+  },
+  {
+    "revision": "ca23807e3f5f5ec669f6a01b336b5a94",
+    "url": "/assignment2/static/media/archspire.ca23807e.jpeg"
+  },
+  {
+    "revision": "ee8bab3a22c375ef2ebddcc1fc5bef5a",
+    "url": "/assignment2/static/media/redcloud.ee8bab3a.jpeg"
+  },
+  {
+    "revision": "d19bba68c982f59a34a6a632f4f868b6",
+    "url": "/assignment2/static/media/twilight.d19bba68.jpeg"
+  }
+]);
